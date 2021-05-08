5f08eaff46eda9af3f002c10788bc1da23aee753
Simon-M20 <75553043+Simon-M20@users.noreply.github.com>
2021-05-08 02:20:55 -0400

    Add files via upload

--- Js/main.js
+++ Js/main.js
@@ -1,1 +1,9 @@
+import alerta from "./modules/alert.js"
+import hamburgerBtn from "./modules/hamburger_btn.js"
 
+
+//DOMContentLoaded
+document.addEventListener("DOMContentLoaded", e => {
+    console.log(alerta)
+    hamburgerBtn(".panel-btn", ".panel", ".nav-bar a")
+})
