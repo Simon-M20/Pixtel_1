290d153d451bb967e3ba701311cf05a4f5c215e2
Simon-M20 <75553043+Simon-M20@users.noreply.github.com>
2021-05-09 00:40:47 -0400

    Update main.js

--- Js/main.js
+++ Js/main.js
@@ -1,9 +1,11 @@
 import alerta from "./modules/alert.js"
 import hamburgerBtn from "./modules/hamburger_btn.js"
+import scrollTopBtn from "./modules/scroll-to-top-btn.js"
 
 
 //DOMContentLoaded
 document.addEventListener("DOMContentLoaded", e => {
     console.log(alerta)
     hamburgerBtn(".panel-btn", ".panel", ".nav-bar a")
+    scrollTopBtn(".scroll-top")
 })
