9f7eabef230e6e094049ad459dcb288a06c1c0d3
Simon-M20 <saimonm0504@gmail.com>
2021-05-12 03:55:33 -0400

    un montón de cambios :c

--- Js/main.js
+++ Js/main.js
@@ -1,6 +1,8 @@
 import alerta from "./modules/alert.js"
 import hamburgerBtn from "./modules/hamburger_btn.js"
 import scrollTopBtn from "./modules/scroll-to-top-btn.js"
+import slider from "./modules/slider.js"
+
 
 
 //DOMContentLoaded
@@ -8,6 +10,8 @@ document.addEventListener("DOMContentLoaded", e => {
     console.log(alerta)
     hamburgerBtn(".panel-btn", ".panel", ".nav-bar a")
     scrollTopBtn(".scroll-top")
+    slider()
+    
 })
 
 window.onload = function() {
